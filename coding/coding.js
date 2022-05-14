750b950406e92183480b584e67f08483df991120
Precious Udegbue <udegbue69@gmail.com>
2022-05-14 12:38:49 +0100

    added sorting algorithm

--- coding/coding.js
+++ coding/coding.js
@@ -5,3 +5,21 @@ const anArray =
 
 console.log(myArray[0]);
 // outputs 1
+
+// sorts listed in my example
+const bubblesort = (array) =>{
+	for (let i = 0; i < array.length; i++) {
+		for (let j = 0; j < array.length; j++) {
+			if (array[j] > array[j + 1]) {
+				let prev = array[j];
+				array[j] = array[j + 1];
+				array[j + 1] = prev;
+			}
+		}
+	}
+	return array;
+}
+
+// Quick sort
+
+const quickSort = 'I am not sure how to implement this but i know it involves using the .sort()';
