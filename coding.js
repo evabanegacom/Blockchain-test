d12e08d15eda9068b4e76ac8fe32a43ffcc9caca
Precious Udegbue <udegbue69@gmail.com>
2022-05-14 11:06:24 +0100

    first commit

---
+++ coding.js
@@ -0,0 +1,7 @@
+const myArray = [1, 2, 3];
+
+const anArray =
+  "An array is a data structure consisting of a collection of elements, each item identified by their index position";
+
+console.log(myArray[0]);
+// outputs 1
